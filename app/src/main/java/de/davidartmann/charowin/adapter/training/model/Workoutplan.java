e242a8218647653f8c1291462105b927658a756f
David <fatmouse69@gmail.com>
2015-10-09 13:53:44 +0200

    added model

---
+++ app/src/main/java/de/davidartmann/charowin/adapter/training/model/Workoutplan.java
@@ -0,0 +1,45 @@
+package de.davidartmann.charowin.adapter.training.model;
+
+/**
+ * Model class for a workout plan.
+ *
+ * Created by David on 08.10.2015.
+ */
+public class WorkoutPlan {
+
+    private String name;
+    private String description;
+    private String amountDays;
+
+    public WorkoutPlan() {}
+
+    public WorkoutPlan(String name, String description, String amountDays) {
+        this.name = name;
+        this.description = description;
+        this.amountDays = amountDays;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAmountDays() {
+        return amountDays;
+    }
+
+    public void setAmountDays(String amountDays) {
+        this.amountDays = amountDays;
+    }
+}
